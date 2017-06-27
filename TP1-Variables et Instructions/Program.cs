ff2876d7a0f3eb07fe9335c44672197037971f2e
Anouk ZOUAOUI <anzouaoui78@yahoo.fr>
2017-06-27 16:35:17 +0200

    TP1-Exercice1

---
+++ TP1-Variables et Instructions/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP1_Variables_et_Instructions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal moyenne = 0;
+            decimal nombre1, nombre2, nombre3;
+
+            Console.WriteLine("Saisir un premier nombre");
+            nombre1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Saisir un deuxième nombre");
+            nombre2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Saisir un troisième nombre");
+            nombre3 = Convert.ToInt32(Console.ReadLine());
+
+            moyenne = (nombre1 + nombre2 + nombre3) / 3;
+            Console.WriteLine("La moyenne est de {0}", moyenne);
+            Console.ReadLine();
+        }
+    }
+}
